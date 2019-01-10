783bf897e112f9a4bacbcbb30badcd06649efe60
Alex Freed <ajfreed00@gmail.com>
2019-01-09 20:00:39 -0600

    Official 2019 version of 2018 code

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -97,4 +97,7 @@ private:
 	frc::SendableChooser<frc::Command*> m_chooser;
 };
 
-START_ROBOT_CLASS(Robot)
+//START_ROBOT_CLASS(Robot)
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
